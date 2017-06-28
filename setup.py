f644ae4773addbd82dcc9b6782c2369ca919f1e8
Calvin Wu <calvin620707@msn.com>
2017-06-28 17:37:12 +0800

    update readme and travis.yml
    1. how to depoly to pypi
    2. only deploy when tag

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='soocii-services-lib',
-    version='1.0.0',
+    version='1.0.1',
     packages=['soocii_services_lib'],
     url='https://github.com/drmobile/services-lib',
     license='Apache Software License',
