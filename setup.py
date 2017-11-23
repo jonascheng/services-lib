eec4f4c949663eb67f36040bd44781756722f7f6
Calvin Wu <calvin620707@msn.com>
2017-11-23 15:15:25 +0800

    import and refine decode encode token

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['click', 'Fabric3', 'boto3', 'awscli'],
+    install_requires=['click', 'Fabric3', 'boto3', 'awscli', 'pycrypto'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
